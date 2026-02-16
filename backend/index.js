d606a530a838042c4f418392e8854e8768505ef9
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-16 23:03:36 +0530

    more issue

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-import app from './app.js';
-import connectDB from './src/db/index.db.js';
 import dotenv from 'dotenv';
-
 dotenv.config({
     path: './.env'
 })
+
+import app from './app.js';
+import connectDB from './src/db/index.db.js';
 
 
 connectDB()
