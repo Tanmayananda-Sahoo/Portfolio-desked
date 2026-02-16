f3749a39fba6a57cf81f417930ac05f390cad41d
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-16 23:19:37 +0530

    final deployment ready

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://portfolio-desked.vercel.app/",
     credentials: true
 }))
 app.use('/user',userRouter);
