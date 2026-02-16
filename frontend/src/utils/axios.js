6176cfd3fc86605065a0b074d4cf8fdeb4e66457
Tanmayananda Sahoo <boyvector06@gmail.com>
2026-02-16 23:14:59 +0530

    frontend deployment ready

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+const API_URL = import.meta.env.VITE_API_URL;
+console.log(API_URL);
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: API_URL,
     withCredentials: true
 })
 
